eb294ae9f80bdd9630101553814ae2ee0447d0a5
Lennysky <ekhinelly@gmail.com>
2022-01-13 08:00:57 +0300

    added styles to links at Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './Navbar.module.css';
+import {NavLink} from "react-router-dom";
 //console.log(classes);
 
 //класс 1 сидит в одной переменной.
@@ -15,36 +16,44 @@ import classes from './Navbar.module.css';
 //будем делать шаблонными строками и эту же запись можем сделать таким образом:
 //мы в строку внедрили джс-код. Пробел - пробел, а class1, class2 - переменные.
 //let classesNew = `${classes.item} ${classes.active}`;
+const dynamicClassName = () => {
+    return (
+    navData => navData.isActive ? classes.active : classes.item
+    )
+}
+
 
 const Navbar = () => {
-return  <nav className={classes.nav}>
-        <div className={classes.item}>
-            <a href="/profile">
-                Profile
-            </a>
-        </div>
-        <div className={`${classes.item} ${classes.active}`}>
-            <a href="/dialogs">
-                Messages
-            </a>
-        </div>
-        <div className={classes.item}>
-            <a href="/news">
-                News
-            </a>
-        </div>
-        <div className={classes.item}>
-            <a href="/music">
-                Music
-            </a>
-        </div>
-        <div className={classes.item}>
-            <a href="/settings">
-                Settings
-            </a>
-        </div>
+    return (
+        <nav className={classes.nav}>
+            <div className={classes.item}>
+                <NavLink to="/profile" className = { navData => navData.isActive ? classes.active : classes.item }>
+                    Profile
+                </NavLink>
+            </div>
+            <div className={`${classes.item} ${classes.active}`}>
+                <NavLink to="/dialogs" className = {dynamicClassName()} >
+                    Messages
+                </NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to="/news" className = {dynamicClassName()}>
+                    News
+                </NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to="/music" className = {dynamicClassName()}>
+                    Music
+                </NavLink>
+            </div>
+            <div className={classes.item}>
+                <NavLink to="/settings" className = {dynamicClassName()}>
+                    Settings
+                </NavLink>
+            </div>
 
-</nav>
+        </nav>
+    )
 }
 
 export default Navbar;
