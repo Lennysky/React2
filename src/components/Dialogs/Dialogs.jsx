74af16b405b91bb6da109f44312c8e7cda587d8f
Lennysky <ekhinelly@gmail.com>
2022-01-15 19:53:23 +0300

    Made grid and class active

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,8 +4,33 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Dimych
+                </div>
+                <div className={classes.dialog}>
+                    Andrey
+                </div>
+                <div className={classes.dialog}>
+                    Elena
+                </div>
+                <div className={classes.dialog}>
+                    Sasha
+                </div>
+                <div className={classes.dialog}>
+                    Victor
+                </div>
+                <div className={classes.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi, you're awesome!</div>
+                <div className={classes.message}>I love you</div>
+                <div className={classes.message}>You're perfect!</div>
+            </div>
+
         </div>
     )
 }
